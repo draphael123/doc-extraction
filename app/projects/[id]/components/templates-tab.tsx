ff1676e8e2450e48b0347e100559c1642901826c
Daniel Raphael <danielraphaelh@gmail.com>
2026-01-05 08:36:57 -0500

    Add toast import to templates tab

--- app/projects/[id]/components/templates-tab.tsx
+++ app/projects/[id]/components/templates-tab.tsx
@@ -7,7 +7,8 @@ import { Input } from '@/components/ui/input'
 import { Label } from '@/components/ui/label'
 import { Dialog, DialogContent, DialogDescription, DialogFooter, DialogHeader, DialogTitle, DialogTrigger } from '@/components/ui/dialog'
 import { Table, TableBody, TableCell, TableHead, TableHeader, TableRow } from '@/components/ui/table'
-import { Plus, FileText, Trash2 } from 'lucide-react'
+import { Plus, FileText, Trash2, Search } from 'lucide-react'
+import { toast } from 'sonner'
 
 interface Template {
   id: string
